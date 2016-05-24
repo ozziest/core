59b6bb984d7298aba6bd38b209bb42edd769f8d8
ozziest <i.ozguradem@gmail.com>
2016-05-24 11:38:10 +0000

    Session sınıfı testleri hazırlandı.

--- tests/Libraries/SessionTest.php
+++ tests/Libraries/SessionTest.php
@@ -12,9 +12,26 @@ class SessionTest extends PHPUnit_Framework_TestCase {
         Mockery::close();
     }
 
-    public function testBlade()
+    /**
+     * @expectedException Ozziest\Core\Exceptions\UserException
+     */
+    public function testException()
     {
-        $session = new Session();
+        $session = new Session(false);
+    }
+    
+    public function testCorrect()
+    {
+        $user = Mockery::mock('user');
+        $user->id = 1;
+        $user->first_name = 'Foo';
+        $user->last_name  = 'Bar';
+        $user->email      = 'foo@bar.com';
+        
+        $session = new Session($user);
+        $this->assertEquals($session->id(), 1);
+        $this->assertEquals($session->name(), 'Foo Bar');
+        $this->assertEquals($session->email(), 'foo@bar.com');
     }
 
 }
