3e1a94cc40ac97853bb3ed2ed7ee548473f41c29
ozziest <i.ozguradem@gmail.com>
2016-06-02 12:43:04 +0000

    Fixed

--- src/Ozziest/Core/Helpers/HttpHelper.php
+++ src/Ozziest/Core/Helpers/HttpHelper.php
@@ -5,29 +5,29 @@ class HttpHelper {
     public static function ip() 
     {
         
-        if (getenv('HTTP_CLIENT_IP'))
+        if (isset($_SERVER['HTTP_CLIENT_IP']))
         {
-            return getenv('HTTP_CLIENT_IP');
+            return $_SERVER['HTTP_CLIENT_IP'];
         } 
-        else if (getenv('HTTP_X_FORWARDED_FOR'))
+        else if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
         {
-            return getenv('HTTP_X_FORWARDED_FOR');
+            return $_SERVER['HTTP_X_FORWARDED_FOR'];
         } 
-        else if(getenv('HTTP_X_FORWARDED'))
+        else if(isset($_SERVER['HTTP_X_FORWARDED']))
         {
-            return getenv('HTTP_X_FORWARDED');
+            return $_SERVER['HTTP_X_FORWARDED'];
         } 
-        else if(getenv('HTTP_FORWARDED_FOR'))
+        else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
         {
-            return getenv('HTTP_FORWARDED_FOR');
+            return $_SERVER['HTTP_FORWARDED_FOR'];
         } 
-        else if(getenv('HTTP_FORWARDED'))
+        else if(isset($_SERVER['HTTP_FORWARDED']))
         {
-           return getenv('HTTP_FORWARDED'); 
+           return $_SERVER['HTTP_FORWARDED']; 
         } 
-        else if(getenv('REMOTE_ADDR'))
+        else if(isset($_SERVER['REMOTE_ADDR']))
         {
-            return getenv('REMOTE_ADDR');
+            return $_SERVER['REMOTE_ADDR'];
         }
             
         return 'UNKNOWN';
